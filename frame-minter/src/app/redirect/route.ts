942f738eafc60d7ba3680e8301a854a9fcd82819
MattWong-ca <matthew.yt.wong@gmail.com>
2024-03-16 11:52:48 -0400

    simple redeploy

--- frame-minter/src/app/redirect/route.ts
+++ frame-minter/src/app/redirect/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse){
   try {
-   return NextResponse.redirect('https://pinata.cloud', { status: 302 }) 
+   return NextResponse.redirect('https://mattwong.ca', { status: 302 }) 
   } catch (error) {
    console.log(error) 
     return NextResponse.json({ error: error })
