f0739a714c6afb5e02815440439221fda7a18bfb
MattWong-ca <matthew.yt.wong@gmail.com>
2024-05-06 09:07:09 -0400

    Continue tutorial

--- galadriel-tutorial/contracts/ChatGpt.sol
+++ galadriel-tutorial/contracts/ChatGpt.sol
@@ -5,6 +5,17 @@ interface IOracle {
     function createLlmCall(uint promptId) external returns (uint);
 }
 
+struct Message {
+    string role;
+    string content;
+}
+
+struct ChatRun {
+    address owner;
+    Message[] messages;
+    uint messagesCount;
+}
+
 contract ChatGpt {
     address private owner;
     address public oracleAddress;
@@ -24,5 +35,28 @@ contract ChatGpt {
     function setOracleAddress(address newOracleAddress) public onlyOwner {
         oracleAddress = newOracleAddress;
         emit OracleAddressUpdated(newOracleAddress);
+    }
+
+    event ChatCreated(address indexed owner, uint indexed chatId);
+    mapping(uint => ChatRun) public chatRuns;
+    uint private chatRunsCount;
+
+    function startChat(string memory message) public returns (uint i) {
+        ChatRun storage run = chatRuns[chatRunsCount];
+
+        run.owner = msg.sender;
+        Message memory newMessage;
+        newMessage.content = message;
+        newMessage.role = "user";
+        run.messages.push(newMessage);
+        run.messagesCount = 1;
+
+        uint currentId = chatRunsCount;
+        chatRunsCount = chatRunsCount + 1;
+
+        IOracle(oracleAddress).createLlmCall(currentId);
+        emit ChatCreated(msg.sender, currentId);
+
+        return currentId;
     }
 }
