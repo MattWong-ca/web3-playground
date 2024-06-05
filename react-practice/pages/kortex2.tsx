da4f6464dba1fe36a8e369310b29962a8c59dff3
MattWong-ca <matthew.yt.wong@gmail.com>
2024-06-05 19:06:02 -0400

    Styling

--- react-practice/pages/kortex2.tsx
+++ react-practice/pages/kortex2.tsx
@@ -14,7 +14,7 @@ export default function Kortex() {
         //     <div style={{ width: '500px', height: '500px', color: 'white', border: '1px solid #8B8B8B', borderRadius: '10px' }}>Test</div>
         // </div>
         <div>
-            <h1>Documents</h1>
+            {/* <h1>Documents</h1>
             <select onChange={(e) => setFilter(e.target.value)}>
                 <option value="">All</option>
                 <option value="Draft">Draft</option>
@@ -30,6 +30,31 @@ export default function Kortex() {
                         <p>{new Date(doc.date_modified).toLocaleDateString()}</p>
                     </div>
                 ))}
+            </div> */}
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '24px' }}>
+                <div>
+                    {sampleDocuments.map((doc, index) => {
+                        return (
+                            <div key={index} style={{ width: '500px', height: '100px', border: '1px solid black', marginBottom: '8px', borderRadius: '10px', padding: '8px', paddingLeft: '12px', paddingRight: '12px' }}>
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <div>{doc.title}</div>
+                                    <div style={{ fontStyle: 'italic'}}>{doc.status}</div>
+                                </div>
+                                <div>
+                                {
+                                    doc.keywords.length > 0 &&
+                                    doc.keywords.map( (word, i) => {
+                                        return (
+                                            <div key={i} style={{ fontSize: '14px', display: 'inline-block', backgroundColor: 'black', color: 'white', width: 'auto', paddingLeft: '6px', paddingRight: '6px', marginRight: '4px', borderRadius: '6px' }}>{word}</div>
+                                        )
+                                    })
+                                }
+                                <div style={{ fontStyle: 'italic', paddingTop: '12px'}}>{doc.content}</div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     );
