013beca52080bdd28450703607853016d1ab0834
MattWong-ca <matthew.yt.wong@gmail.com>
2025-08-22 20:41:36 -0700

    remove frames template

--- another-v2-template/src/lib/constants.ts
+++
@@ -1,3 +0,0 @@
-export const PROJECT_ID = 'farcaster-frames-template';
-export const PROJECT_TITLE = "Farcaster Frames Template";
-export const PROJECT_DESCRIPTION = "A Farcaster Frames v2 Template by hellno";
